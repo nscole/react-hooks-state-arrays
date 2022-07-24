6aa830ca7103a33237f0384548dcfb401e5983a6
Naomi Cole <naomisarahcole@gmail.com>
2022-07-24 15:44:39 +1000

    cleaned up extraneous function

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewSpicyFood } from "../data";
+import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 // ORIGINAL FUNCTION
 /*function SpicyFoodList() {
@@ -18,8 +18,9 @@ import { spicyFoods, getNewSpicyFood } from "../data";
   );
 }*/
 
+
 // Adding Elements to an Array
-function SpicyFoodList() {
+/*function SpicyFoodList() {
 const [foods, setFoods] = useState(spicyFoods);
 const foodList = foods.map((food) => (
   <li key={food.id}>
@@ -38,10 +39,119 @@ return (
     <button onClick={handleAddFood}>Add New Food</button>
     <ul>{foodList}</ul>
   </div>
-); };
+); };*/
 
 
+// Removing from an Arry
+/*function SpicyFoodList() {
+  const [foods, setFoods] = useState(spicyFoods);
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  )); 
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+    // console.log("removed");
+  }; 
+
+  return (
+    <div>
+      <button onClick={handleLiClick}>Remove Food</button>
+      <ul>{foodList}</ul>
+    </div>
+  ); };*/
+
+  
+// Updating Elements in an array
+/*function SpicyFoodList() {
+  const [foods, setFoods] = useState(spicyFoods);
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  )); 
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  } 
+
+  return (
+    <div>
+      <button onClick={handleLiClick}>Update Food</button>
+      <ul>{foodList}</ul>
+    </div>
+  ); };*/
 
 
+// Working With Multiple State Variables
+/*function SpicyFoodList() {
+const [foods, setFoods] = useState(spicyFoods);
+const [filterBy, setFilterBy] = useState("All");
+
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+}); 
+function handleLiClick(id){
+function handleFilterChange(event) {
+  setFilterBy(event.target.value);
+}
+
+<select name="filter" onChange={handleFilterChange}>
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Sichuan">Sichuan</option>
+  <option value="Thai">Thai</option>
+  <option value="Mexican">Mexican</option>
+</select>; 
+
+   const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  )); 
+
+  return (
+    <div>
+      <button onClick={handleLiClick}>Update Food</button>
+      <ul>{foodList}</ul>
+    </div>
+  );};};*/
+
+  function SpicyFoodList() {
+    const [foods, setFoods] = useState(spicyFoods);
+  
+    function handleAddFood() {
+      const newFood = getNewRandomSpicyFood();
+      console.log(newFood);
+    }
+  
+    const foodList = foods.map((food) => (
+      <li key={food.id}>
+        {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      </li>
+    ));
+  
+    return (
+      <div>
+        <button onClick={handleAddFood}>Add New Food</button>
+        <ul>{foodList}</ul>
+      </div>
+    );
+  }
 
 export default SpicyFoodList;
--- src/data/index.js
+++ src/data/index.js
@@ -3,14 +3,21 @@ const spicyFoods = [
   { id: 2, name: "Mapo Tofu", cuisine: "Sichuan", heatLevel: 6 },
 ];
 
+let nextId = 3;
+
 const newSpicyFoods = [
-  { id: 3, name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
-  { id: 4, name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
-  { id: 5, name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
+  { name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
+  { name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
+  { name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
 ];
 
-function getNewSpicyFood() {
-  return newSpicyFoods.shift();
+// Get one random spicy food from the array
+function getNewRandomSpicyFood() {
+  const index = Math.floor(Math.random() * newSpicyFoods.length);
+  const newSpicyFood = { ...newSpicyFoods[index] };
+  newSpicyFood.id = nextId;
+  nextId++;
+  return newSpicyFood;
 }
 
-export { spicyFoods, getNewSpicyFood };
+export { spicyFoods, getNewRandomSpicyFood };
